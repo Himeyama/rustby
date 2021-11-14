1885ac52b546de4724422691aa6c1731bfaf28d0
Murata Mitsuharu <hikari.photon+dev@gmail.com>
2021-11-14 22:13:19 +0900

    .

--- ext/rustby/extconf.rb
+++ ext/rustby/extconf.rb
@@ -1,8 +1,1 @@
 # frozen_string_literal: true
-
-require 'fileutils'
-
-# ext_dir = Dir.pwd
-# FileUtils.mkdir_p ext_dir
-# src = %w[Makefile Cargo.toml Cargo.lock src].map { |file| "#{File.dirname($PROGRAM_NAME)}/#{file}" }
-# FileUtils.cp_r src, ext_dir
