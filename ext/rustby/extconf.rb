70370ae5cfd075bc8c4e93240e51f4aa26bbeec3
Murata Mitsuharu <hikari.photon+dev@gmail.com>
2021-11-14 22:11:06 +0900

    .

--- ext/rustby/extconf.rb
+++ ext/rustby/extconf.rb
@@ -2,7 +2,7 @@
 
 require 'fileutils'
 
-ext_dir = Dir.pwd
-FileUtils.mkdir_p ext_dir
-src = %w[Makefile Cargo.toml Cargo.lock src].map { |file| "#{File.dirname($PROGRAM_NAME)}/#{file}" }
-FileUtils.cp_r src, ext_dir
+# ext_dir = Dir.pwd
+# FileUtils.mkdir_p ext_dir
+# src = %w[Makefile Cargo.toml Cargo.lock src].map { |file| "#{File.dirname($PROGRAM_NAME)}/#{file}" }
+# FileUtils.cp_r src, ext_dir
